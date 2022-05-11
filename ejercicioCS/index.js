09bb42ce5db5bd12596790c9e8d993f05d5638e1
julbrek13 <julianmacarron@hotmail.com>
2022-05-11 15:16:43 -0300

    filtro/desestructuro la info sacada de la url

--- ejercicioCS/index.js
+++ ejercicioCS/index.js
@@ -1,6 +1,10 @@
 const axios = require("axios");
 const main = async () => {
 let response = await axios.get("https://rickandmortyapi.com/api/character");
-console.log(response);
-}
+//desestructuro data de nuestra respuesta(la que nos dio por terminal la variable response)  saco results
+let response = {data:{results}};
+
+console.log(results);
+
 main();
+}
