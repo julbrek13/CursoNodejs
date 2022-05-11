0dc265dc4b7296a80a462cb73c2b4f9151066ec7
julbrek13 <julianmacarron@hotmail.com>
2022-05-11 15:23:44 -0300

    filtro solo los results de la url

--- ejercicioCS/index.js
+++ ejercicioCS/index.js
@@ -2,9 +2,15 @@ const axios = require("axios");
 const main = async () => {
 let response = await axios.get("https://rickandmortyapi.com/api/character");
 //desestructuro data de nuestra respuesta(la que nos dio por terminal la variable response)  saco results
-let response = {data:{results}};
+//declaro una variable con el contenido de respons y las especificaciones de data:results
+let {data:{results}} = response ;
+//otro filtro mas pero ahora utilizo el apartado results.
+let characters = results.map((character) => {
+return{
+id: character.id,
+name: character.name,};
 
-console.log(results);
-
+});
+console.log(characters);
+};
 main();
-}
