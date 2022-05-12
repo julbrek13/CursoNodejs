3fdd03ba22559f1de165566e4a530e3f595f6d47
julbrek13 <julianmacarron@hotmail.com>
2022-05-12 19:29:36 -0300

    editamos el package.json  en los scripts para poder ejecutar nodemon

--- ServidoresWeb/index.js
+++ ServidoresWeb/index.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const { request } = require("http");
+//almaceno todos los datos del json mockdata
+const data = require("./MOCK_DATA.json");
+
 //EJECUTARE EXPRES LLAMANDO A EXpres desde esta app
 const app = express();
 const PORT = 3000;
@@ -7,7 +10,9 @@ const PORT = 3000;
 //recibe como paramtro la ruta y el controlador de la ruta.
 //req= request(peticion del cliente))y res = response(la respuesta que enviaras al cliente)
 app.get("/", (req, res) => {
-  res.json({ message: "hola mundo desde express" });
+  //pruebo que devuelva data
+  res.json({ data });
+  //hay que cerrar y volver a abrir el servidor para ver los cambios(para que no suceda esto usaremos nodemon a constinucacion)
 });
 
 //listen recibe un  un puerto que nos indica cuando este levantado y un callback
