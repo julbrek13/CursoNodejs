c0e938d626a194e0aa267bb6dcafda3c881c54df
julbrek13 <julianmacarron@hotmail.com>
2022-05-12 11:29:51 -0300

    explicamos la y alñadimos la libreria path y hacemops pruebas

--- ejercicioCS/index.js
+++ ejercicioCS/index.js
@@ -1,4 +1,8 @@
 const axios = require("axios");
+//se guarda en una constante porque no debe ser modificado un paquete y le damos un .promises al paquete para traerlo con esa funcionalidad. 
+const fs = require('fs').promises;
+//path nos ayudara con las rutas donde almacenaremos nuestros archivos.
+const path = require('path');
 const main = async () => {
 let response = await axios.get("https://rickandmortyapi.com/api/character");
 //desestructuro data de nuestra respuesta(la que nos dio por terminal la variable response)  saco results
@@ -13,7 +17,11 @@ status: character.status,
 species: character.species,
 };
 //añado un nuevo map para que solo retorne lo valores
-}).map((personaje) => Object.values(personaje).join(', '));
+}).map((personaje) => Object.values(personaje).join(', ')).join("/n");
 console.log(characters);
+//devuelve el path en donde nos enmcontramos.
+console.log(__dirname);
+//crear un path con argumentos usando joint
+console.log(path.join(__dirname, 'data.csv'));
 };
 main();
